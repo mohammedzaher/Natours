aee23d998fd8cd160e5ad85a2925f93f62cc2771
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-16 06:55:10 +0300

    Import Data using import-dev-data.js script

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+tours.forEach((el) => {
+  el.createdAt = Date.now();
+  delete el.id;
+});
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
