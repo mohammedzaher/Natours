df5bf058367f86bf86584e4b642132980877b53c
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-21 06:24:34 +0300

    Add pre aggregate Hook

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,6 +100,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// AGGREGATION MIDDLEWARE
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
 // tourSchema.post(/^find/, function (docs, next) {
 //   // console.log(docs);
 //   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
