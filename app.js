92d575d05666ddf66fe91fb6904540037987bac4
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-22 09:47:35 +0300

    Handle unhandled routes

--- app.js
+++ app.js
@@ -20,4 +20,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
