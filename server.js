814111dbd75cdba859ecbbdd5ee60a9fd4d2b46c
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-22 14:32:35 +0300

    Handle Uncaught Exceptions, UnhandledRejections

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
