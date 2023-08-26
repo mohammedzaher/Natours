179c95457662959896f0998dee2fdc824d1541cc
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-26 12:34:33 +0300

    Add user model schema

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    // This only works on CREATE and SAVE!!!
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
