a1439afceea655a310721ab37be3f5c4a849ab9e
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-26 13:35:42 +0300

    Add authentication controller

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); // serving static files
 
