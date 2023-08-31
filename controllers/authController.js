c787bb950c6ef3c23d3466ebf9027af4d0947bc2
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-08-31 15:51:10 +0300

    Send JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,17 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000, // 90 days
+    ),
+    httpOnly: true, // cannot be accessed or modified in any way by the browser
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined; // remove the password from the output
 
   res.status(statusCode).json({
     status: 'success',
