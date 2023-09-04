dbbbe33cc8802dfde4a665d872b5d88e6e394125
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-09-04 17:09:54 +0300

    Add reviews routs get and post

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -71,6 +72,7 @@ app.use((req, res, next) => {
 // 2) ROUTES MIDDLEWARES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
