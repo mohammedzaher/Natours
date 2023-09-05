78fa89ea7947335bee629169c94f953570751817
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-09-05 23:56:10 +0300

    Implement calculates distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -38,6 +38,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
@@ -88,7 +89,7 @@ const tourSchema = new mongoose.Schema(
         default: 'Point',
         enum: ['Point'],
       },
-      coordinats: [Number],
+      coordinates: [Number],
       address: String,
       description: String,
     },
@@ -100,7 +101,7 @@ const tourSchema = new mongoose.Schema(
           default: 'Point',
           enum: ['Point'],
         },
-        coordinats: [Number],
+        coordinates: [Number],
         address: String,
         description: String,
         day: Number,
@@ -120,8 +121,9 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
-tourSchema.index({ price: 1, ratingsAverage: -1 });
-tourSchema.index({ slug: 1 });
+// tourSchema.index({ price: 1, ratingsAverage: -1 });
+// tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -154,14 +156,6 @@ tourSchema.pre(/^find/, function (next) {
   // tourSchema.pre('find', function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
-  next();
-});
-
-// AGGREGATION MIDDLEWARE
-
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  // console.log(this.pipeline());
   next();
 });
 
