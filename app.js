bbab5353fc015aabfd2eed2e0669a557c98836d5
Mohammed Zaher <mohammed.zaaheer9797@gmail.com>
2023-09-13 17:51:49 +0300

    Add helmet set security HTTP headers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views')); // set the views folder
 app.use(cors()); // Access-Control-Allow-Origin *
 app.options('*', cors()); // pre-flight phase
 // Set security HTTP headers
-// app.use(helmet());
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
